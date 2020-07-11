ca47e0f3f5e413a201af90253eb75a83344ebac3
Alvin Piter <alpiter10@gmail.com>
2020-07-11 11:33:01 +0800

    CF submission entity (#1)
    * Codeforces Submission enitity
    
    * Fix Problem interface method's signature

--- entities/general.go
+++ entities/general.go
@@ -4,9 +4,17 @@ package entities
 Problem is an interface that represents an online judge problem
 */
 type Problem interface {
-	getID() string
-	getName() string
-	getRating() int
-	getTags() []string
-	getURL() string
+	GetID() string
+	GetName() string
+	GetRating() int
+	GetTags() []string
+	GetURL() string
+}
+
+/*
+Submission is an interface that represents an online judge submission
+*/
+type Submission interface {
+	GetProblemID() string
+	IsAccepted() bool
 }
