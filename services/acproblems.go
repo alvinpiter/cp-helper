e629f2887d535918e0318762479b443d8cf47df5
Alvin Piter <alpiter10@gmail.com>
2020-07-11 20:23:02 +0800

    Implement service to get accepted problems for particular handle (#4)

---
+++ services/acproblems.go
@@ -0,0 +1,37 @@
+package services
+
+import (
+	"errors"
+
+	"github.com/alvinpiter/cp-helper/entities"
+)
+
+func (s *Service) GetAcceptedProblems(oj, handle string) ([]entities.Problem, error) {
+	var submissions []*entities.CodeforcesSubmission
+	var err error
+
+	switch oj {
+	case "codeforces":
+		submissions, err = s.CodeforcesRepo.GetSubmissions(handle)
+	default:
+		return nil, errors.New("Unknown online judge")
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	acProblems := []entities.Problem{}
+	seenID := make(map[string]bool) //To avoid duplicates
+	for _, submission := range submissions {
+		if submission.IsAccepted() {
+			problemID := submission.GetProblem().GetID()
+			if _, seen := seenID[problemID]; !seen {
+				acProblems = append(acProblems, submission.GetProblem())
+				seenID[problemID] = true
+			}
+		}
+	}
+
+	return acProblems, nil
+}
